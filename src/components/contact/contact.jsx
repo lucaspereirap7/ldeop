07b52ef2ed9a3f03b8a5a36fd652458b5af795a0
Lucas Pereira <lucas.pereira17@estudante.ufla.br>
2024-02-18 00:34:37 -0300

    aAH

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -5,6 +5,19 @@ const Contact = () => {
     return (
         <div className="contact">
             <h1 className="aboutTitle">Contact</h1>
+            <div className="contactInfo">
+                <h4>
+                Hello! I'm a programming enthusiast, a music lover, and a football enthusiast. Currently, I'm on my journey as a developer and student, with two years of experience in the mgCode team and in the fifth semester of the Information Systems college at UFLA. If you're looking to discuss ideas about the programming world, share musical insights, or debate about the latest happenings in the football world, I'm here! Feel free to contact me through my social media below!
+                </h4>
+            </div>
+            <div className="socialIcons">
+                <div className='icons'>
+                <ion-icon className="iconInstagram" name="logo-instagram"></ion-icon>
+                <ion-icon className="iconLinkedin" name="logo-linkedin"></ion-icon>
+                <ion-icon className="iconGithub" name="logo-github"></ion-icon>
+                </div>
+                <h5>©️ copyright 2024</h5>
+            </div>
         </div>
     );
 }
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import "./projects.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import huay from "../../projects/huay.png";
+import nai from "../../projects/nai.png";
+import priorite from "../../projects/priorite.png";
+import facces from "../../projects/facces.png";
 
 const Projects = () => {
+    const slides = [nai, priorite, huay, facces];
+    const captions = [
+        "Nai - Administration System",
+        "Priorite Odonto - Web Page",
+        "Huay - Administration System",
+        "Facces Odonto - Web Page"
+    ];
+
     return (
         <div className="projects">
             <h1 className="aboutTitle tracking-in-expand-fwd-top">Projects</h1>
+            <div>
+                <Swiper
+                    pagination={{ clickable: true }}
+                    slidesPerView={1} // Defina o número de slides visíveis
+                    spaceBetween={30}
+                >
+                    {slides.map((slide, index) => (
+                        <SwiperSlide key={index}>
+                            <div className="slide-container">
+                                <img className="projectsImgs" src={slide} alt={`Slide ${index}`} />
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+            <h4>
+                Projects carried out as a team. All rights reserved.
+            </h4>
         </div>
     );
 }
